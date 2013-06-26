beaa45740687d295924421c8ad27c4cd7d673e80
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-25 20:20:45 -0500

    Admin spec page tests

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -2,9 +2,35 @@ require 'spec_helper'
 
 describe 'Admin' do
   context "on admin homepage" do
-    it "can see a list of recent posts"
-    it "can edit a post by clicking the edit link next to a post"
-    it "can delete a post by clicking the delete link next to a post"
+
+    before(:each) do
+      @post = Post.new 
+      @post.title = "Abi ain't my Daddy"
+      @post.content = "Having it"
+      @post.is_published = true
+      @post.save
+      page.driver.browser.basic_authorize('geek', 'jock')
+    end
+
+    it "can see a list of recent posts" do
+      visit admin_posts_url
+      page.should have_content(@post.title)
+    end
+
+    it "can edit a post by clicking the edit link next to a post" do
+      visit admin_posts_url
+      click_link 'Edit'
+      expect(page).to have_content "Publish?"
+      expect(page).to have_content @post.title
+    end
+
+    it "can delete a post by clicking the delete link next to a post" do
+      visit admin_posts_url
+      expect(page).to have_content @post.title
+      click_link 'Delete'
+      expect(page).to have_no_content @post.title
+    end
+
     it "can create a new post and view it" do
        visit new_admin_post_url
 
@@ -22,7 +48,11 @@ describe 'Admin' do
 
   context "editing post" do
     it "can mark an existing post as unpublished" do
-      pending # remove this line when you're working on implementing this test
+      visit admin_posts_url
+      p page.html
+      click_link 'Edit'
+      uncheck('is_published')
+      click_button 'Save'
 
       page.should have_content "Published: false"
     end
@@ -31,6 +61,6 @@ describe 'Admin' do
   context "on post show page" do
     it "can visit a post show page by clicking the title"
     it "can see an edit link that takes you to the edit post path"
-    it "can go to the admin homepage by clicking the Admin welcome page link"
+    it "change go to the admin homepage by clicking the Admin welcome page link"
   end
 end
